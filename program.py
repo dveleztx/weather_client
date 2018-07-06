4f5c65fa7e1213f61c87c98a63c5a22f338cdbd6
dveleztx <dveleztx@gmail.com>
2018-07-06 14:05:31 -0700

    Now printing weather for whatever zipcode is entered in the weather app

--- program.py
+++ program.py
@@ -1,15 +1,32 @@
 import requests
 import bs4
+import collections
 
+WeatherReport = collections.namedtuple('WeatherReport',
+                                       'cond, temp, scale, loc')
 
 def main():
+    # t = (1, 7, 'cat', [1, 2, 3])
+    # print(t)
+    # print(t[1])
+
+    # n1, n2, s, l = t
+    # print(n1, n2, s, l)
+    # return
+
     print_the_header()
 
     code = input('What zipcode do you want the weather (97201)? ')
 
     html = get_html_from_web(code)
+    report = get_weather_from_html(html)
 
-    get_weather_from_html(html)
+    print('The temp in {} is {} {} and {}'.format(
+        report.loc,
+        report.temp,
+        report.scale,
+        report.cond
+    ))
 
     # display for the forecast
 
@@ -37,7 +54,34 @@ def get_weather_from_html(html):
     # weatherConditionCss = '.condition-icon'
 
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    # print(soup)
+    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_='condition-icon').get_text()
+    temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
+    scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
+
+    loc = cleanup_text(loc)
+    loc = find_city_and_state_from_location(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    # print(condition, temp, scale, loc)
+    # return condition, temp, scale, loc
+    report = WeatherReport(cond=condition, temp=temp, scale=scale, loc=loc)
+    return report
+
+
+def cleanup_text(text: str):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
+
+
+def find_city_and_state_from_location(loc: str):
+    parts = loc.split('\n')
+    return parts[0].strip()
 
 
 if __name__ == '__main__':
